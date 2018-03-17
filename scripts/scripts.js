ecfab333a20a5e2f7bb4c0ac54959e815bd479de
Horjea Cosmin <horjeacosmin@yahoo.com>
2018-03-17 13:10:09 +0200

    Added footer and tweaks

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+  var url = "https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyD0ScpREJAM-2Y9iG8Bj1YHHg-LfArMoZs";
   $('.modal').modal('hide');
 
   $('.image-showcase').on("click",function(){
