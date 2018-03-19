aca048a81317224af7de39531925f54e0b967f9b
Horjea Cosmin <horjeacosmin@yahoo.com>
2018-03-19 16:46:03 +0200

    Buggy nav and improvements

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,27 @@
 $( document ).ready(function() {
-  var url = "https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyD0ScpREJAM-2Y9iG8Bj1YHHg-LfArMoZs";
   $('.modal').modal('hide');
+  var btn = $('.nav-toggle');
+  var nav = $('.navbar-nav');
 
   $('.image-showcase').on("click",function(){
     var source= $(this).children().first().attr("src");
     var title= $(this).children().first().attr("alt");
+    var desc= $(this).children().first().attr("desc");
 
     $( ".modal" ).modal('show');
     $('.modal-title').text(title);
     $('.modal-image').attr('src',source);
+    $('.modal-text').text(desc);
+  });
+
+  btn.on('click',function(){
+
+    nav.toggle({
+      "duration":200,
+    });
+  });
+  $( window ).resize(function() {
+    nav.
   });
 
 });
